029c1b14bb9be178c156b4350839074e3ebf7cb5
FO-nTTaX <fo.nttax@yahoo.de>
2016-02-22 17:24:08 +0100

    remove hackish way to link to the API

--- js/trendingmenu.js
+++ js/trendingmenu.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     
     /* script to fill the trending pages menu */
     if($('#trending-pages-menu').length) {
-        $.getJSON(mw.config.get('wgScriptPath')+'/api.php?action=trendingmenu&format=json', function (result) {
+        $.getJSON(mw.util.wikiScript('api')+'?action=trendingmenu&format=json', function (result) {
             var html = '';
             for(var i = 0; i < 5; i++) {
                 html += '<li><a href="'+result.trendingmenu[i].href+'">'+result.trendingmenu[i].text+'</a></li>';
