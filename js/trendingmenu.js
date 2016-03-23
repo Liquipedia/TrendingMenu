3ffd8ae2dbd20f9dd57b57768524f39f7e4d415d
Richard Stanway <r1ch@teamliquid.net>
2016-03-24 02:05:19 +0900

    Fix error when the number of results is low

--- js/trendingmenu.js
+++ js/trendingmenu.js
@@ -5,7 +5,8 @@ $(document).ready(function() {
         $.getJSON(mw.util.wikiScript('api')+'?action=trendingmenu&format=json', function (result) {
             var html = '';
             for(var i = 0; i < 5; i++) {
-                html += '<li><a href="'+result.trendingmenu[i].href+'">'+result.trendingmenu[i].text+'</a></li>';
+                if (result.trendingmenu[i])
+                    html += '<li><a href="'+result.trendingmenu[i].href+'">'+result.trendingmenu[i].text+'</a></li>';
             }
             $('#trending-pages-menu').html(html);
         });
