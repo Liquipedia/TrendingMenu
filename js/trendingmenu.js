aa78d12c556a19f83b36b47187b24fd744421ed3
FO-nTTaX <fo.nttax@yahoo.de>
2017-04-25 19:54:39 +0200

    set uselang parameter to not have Cache-Control private

--- js/trendingmenu.js
+++ js/trendingmenu.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	/* script to fill the trending pages menu */
 	if($('#trending-pages-menu').length) {
-		$.getJSON(mw.util.wikiScript('api')+'?action=trendingmenu&format=json', function (result) {
+		$.getJSON(mw.util.wikiScript('api')+'?action=trendingmenu&uselang=content&format=json', function (result) {
 			var html = '';
 			for(var i = 0; i < 5; i++) {
 				if (result.trendingmenu[i]) {
