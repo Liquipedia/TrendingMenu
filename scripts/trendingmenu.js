0f4c44b97d40ed37e7d05aeb65c0846db52f811f
Alex Winkler <FO-nTTaX@users.noreply.github.com>
2017-09-25 01:45:08 +0200

    No need to wait for document ready

--- scripts/trendingmenu.js
+++ scripts/trendingmenu.js
@@ -1,17 +1,14 @@
-$( document ).ready( function() {
-	mw.loader.using( 'mediawiki.util' ).then( function() {
-
-		/* script to fill the trending pages menu */
-		if( $( '#trending-pages-menu' ).length ) {
-			$.getJSON( mw.util.wikiScript('api') + '?action=trendingmenu&uselang=content&format=json', function ( result ) {
-				var html = '';
-				for( var i = 0; i < 5; i++ ) {
-					if( result.trendingmenu[i] ) {
-						html += '<li><a href="' + result.trendingmenu[i].href + '">' + result.trendingmenu[i].text + '</a></li>';
-					}
+mw.loader.using( 'mediawiki.util' ).then( function() {
+	/* script to fill the trending pages menu */
+	if( $( '#trending-pages-menu' ).length ) {
+		$.getJSON( mw.util.wikiScript('api') + '?action=trendingmenu&uselang=content&format=json', function ( result ) {
+			var html = '';
+			for( var i = 0; i < 5; i++ ) {
+				if( result.trendingmenu[i] ) {
+					html += '<li><a href="' + result.trendingmenu[i].href + '">' + result.trendingmenu[i].text + '</a></li>';
 				}
-				$( '#trending-pages-menu' ).html( html );
-			} );
-		}
-	} );
+			}
+			$( '#trending-pages-menu' ).html( html );
+		} );
+	}
 } );
