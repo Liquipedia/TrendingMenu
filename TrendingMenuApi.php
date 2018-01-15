963e1148ceb59a86c295b443cf47121d7d6984a2
FO-nTTaX <fo.nttax@yahoo.de>
2018-01-15 13:37:17 +0100

    refactor to MediaWiki DB interface

--- TrendingMenuApi.php
+++ TrendingMenuApi.php
@@ -1,48 +1,23 @@
 <?php
 
 class TrendingMenuApi extends ApiBase {
-	public static function get_db_object() {
-		global $wgDBtype,
-			$wgDBserver,
-			$TL_DB,
-			$wgDBuser,
-			$wgDBpassword;
-		$db = null;
-		try {
-			$db = new PDO( $wgDBtype . ':host=' . $wgDBserver. ';dbname=' . $TL_DB,
-				$wgDBuser, $wgDBpassword );
-			$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-			$db->setAttribute( PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC );
-			$db->setAttribute( PDO::ATTR_EMULATE_PREPARES, false );
-		} catch( PDOException $e ) {
-			// echo "Connection Error: " . $e->getMessage();
-		}
-		return $db;
-	}
 	public function execute() {
-		global $wgScriptPath;
+		global $wgScriptPath, $TL_DB;
 		// Tell squids to cache
 		$this->getMain()->setCacheMode( 'public' );
 		// Set the squid & private cache time in seconds
 		$this->getMain()->setCacheMaxAge( 300 );
-		$trendingArticles = array ();
+		$trendingArticles = array();
 
-		$db = self::get_db_object();
-		if( $db == null ) {
-			return null;
-		}
-		$pdostatement = $db->prepare( "SELECT * FROM `wiki_hot` WHERE `wiki` = :wiki ORDER BY hits DESC LIMIT 5" );
-		$pdostatement->execute( ['wiki' => substr($wgScriptPath, 1)] );
-		$rows = $pdostatement->fetchAll();
-
-		foreach( $rows as $row ) {
-			$title = $row['title'];
-			$title = str_replace ("_", " ", $title);
-			$url = $row['page'];
-			$trendingArticles[] = array (
-				'text' => $title,
-				'href' => $url
-			);
+		$dbr = wfGetDB( DB_REPLICA, '', $TL_DB );
+		$res = $dbr->select( 'wiki_hot', '*', array( 'wiki' => substr( $wgScriptPath, 1 ) ), __METHOD__, array( 'order' => 'hits DESC', 'limit' => 5 ) );
+		if( $dbr->numRows( $res ) ) {
+			while( $row = $res->fetchObject() ) {
+				$trendingArticles[] = array (
+					'text' => str_replace( '_', ' ', $row->title ),
+					'href' => $row->page,
+				);
+			}
 		}
 
 		$this->getResult()->addValue( null, $this->getModuleName(), $trendingArticles );
@@ -64,8 +39,7 @@ class TrendingMenuApi extends ApiBase {
 
 	public function getExamplesMessages() {
 		return array(
-			'action=trendingmenu&format=xml'
-			=> 'trendingmenuapi-example'
+			'action=trendingmenu&format=xml' => 'trendingmenuapi-example'
 		);
 	}
 }
