d1c8affa3ba1502bf907305ed3f1da3f891d94ae
FO-nTTaX <fo.nttax@yahoo.de>
2018-01-15 13:36:59 +0100

    code style

--- scripts/trendingmenu.js
+++ scripts/trendingmenu.js
@@ -1,7 +1,7 @@
 mw.loader.using( 'mediawiki.util' ).then( function() {
 	/* script to fill the trending pages menu */
 	if( $( '#trending-pages-menu' ).length ) {
-		$.getJSON( mw.util.wikiScript('api') + '?action=trendingmenu&uselang=content&format=json', function ( result ) {
+		$.getJSON( mw.util.wikiScript( 'api' ) + '?action=trendingmenu&uselang=content&format=json', function ( result ) {
 			var html = '';
 			for( var i = 0; i < 5; i++ ) {
 				if( result.trendingmenu[i] ) {
