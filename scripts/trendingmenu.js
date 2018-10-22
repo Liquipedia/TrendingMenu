73ca906777d0738742d0cbb182c40e1e759c145b
FO-nTTaX <fo.nttax@yahoo.de>
2018-10-22 14:26:47 +0200

    cleanup and performance, also fixed a edge case bug with skins

--- scripts/trendingmenu.js
+++ scripts/trendingmenu.js
@@ -1,19 +1,40 @@
-mw.loader.using( 'mediawiki.util' ).then( function() {
-	/* script to fill the trending pages menu */
-	if ( $( '#trending-pages-menu' ).length ) {
-		$.getJSON( mw.util.wikiScript( 'api' ) + '?action=trendingmenu&uselang=content&format=json', function( result ) {
-			var html = '';
-			for ( var i = 0; i < 5; i++ ) {
-				var skin = mw.user.options.get( 'skin' );
-				if ( result.trendingmenu[i] ) {
-					if ( skin === 'bruinen' ) {
-						html += '<a class="dropdown-item" href="' + result.trendingmenu[i].href + '">' + result.trendingmenu[i].text + '</a>';
-					} else {
-						html += '<li><a href="' + result.trendingmenu[i].href + '">' + result.trendingmenu[i].text + '</a></li>';
-					}
-				}
+( function( window, document, mw ) {
+	'use strict';
+	var trendingmenu = {
+		init: function() {
+			if ( document.readyState === 'loading' ) {
+				window.addEventListener( 'DOMContentLoaded', trendingmenu.run );
+			} else {
+				trendingmenu.run();
 			}
-			$( '#trending-pages-menu' ).html( html );
-		} );
-	}
-} );
+		},
+		run: function() {
+			mw.loader.using( 'mediawiki.util' ).then( function() {
+				var menuItem = document.getElementById( 'trending-pages-menu' );
+				if ( menuItem !== null ) {
+					var api = new mw.Api();
+					api.get( {
+						action: 'trendingmenu',
+						uselang: 'content',
+						format: 'json'
+					} ).done( function( data ) {
+						var html = '';
+						for ( var i = 0; i < 5; i++ ) {
+							var skin = mw.config.get( 'skin' );
+							if ( data.trendingmenu[i] ) {
+								if ( skin === 'bruinen' ) {
+									html += '<a class="dropdown-item" href="' + data.trendingmenu[i].href + '">' + data.trendingmenu[i].text + '</a>';
+								} else {
+									html += '<li><a href="' + data.trendingmenu[i].href + '">' + data.trendingmenu[i].text + '</a></li>';
+								}
+							}
+						}
+						menuItem.innerHTML = html;
+					} );
+				}
+			} );
+		}
+	};
+	trendingmenu.init();
+
+}( window, document, mw ) );
